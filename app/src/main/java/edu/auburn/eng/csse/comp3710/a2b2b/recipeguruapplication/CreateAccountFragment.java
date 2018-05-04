4977c520ee06733efec9acdc4aede4b15b08e63b
MasonMonday <Pingst0n>
2018-05-03 19:22:02 -0500

    Mason's Stuff

---
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/a2b2b/recipeguruapplication/CreateAccountFragment.java
@@ -0,0 +1,16 @@
+package edu.auburn.eng.csse.comp3710.a2b2b.recipeguruapplication;
+
+import android.support.v4.app.Fragment;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class CreateAccountFragment extends Fragment {
+    private Account myNewAccount;
+    private EditText myFirstNameCreate;
+    private EditText myLastNameCreate;
+    private EditText myEmailCreate;
+    private EditText myPasswordCreate;
+    private EditText myPasswordConfirm;
+    private Button mySaveButton;
+
+}
