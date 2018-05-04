942d6919028ab2fdd298a73eb0ac78717d0a5df0
MasonMonday <Pingst0n>
2018-05-04 02:50:47 -0500

    Mason's Changes to Files by Deleting LoginFragment and update CreateAccountFragment

---
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/a2b2b/recipeguruapplication/IngredientFragment.java
@@ -0,0 +1,12 @@
+package edu.auburn.eng.csse.comp3710.a2b2b.recipeguruapplication;
+
+import android.support.v4.app.Fragment;
+
+public class IngredientFragment extends Fragment {
+
+    private String myText;
+    private int myAmount;
+    private int myRecipeID;
+    private int myMeasurementID;
+
+}
