4837e37b1ce56633fb8c5accc97a498b3e8e0991
MasonMonday <Pingst0n>
2018-05-03 23:42:49 -0500

    Mason's Changes to CreateAccountFragment

--- app/src/main/java/edu/auburn/eng/csse/comp3710/a2b2b/recipeguruapplication/LoginFragment.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/a2b2b/recipeguruapplication/LoginFragment.java
@@ -1,80 +1,96 @@
 package edu.auburn.eng.csse.comp3710.a2b2b.recipeguruapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import edu.auburn.eng.csse.comp3710.a2b2b.recipeguruapplication.Entity.Account;
+
+import static com.google.android.gms.common.util.WorkSourceUtil.TAG;
 
 public class LoginFragment extends Fragment {
 
     private Account myTestAccount;
-    private EditText myEmailLogin;
-    private EditText myPasswordLogin;
-    private Button myLoginButton;
-    private Button myCreateAccountButton;
-    private Button myForgotPasswordButton;
+    private GoogleSignInClient myGoogleSignInClient;
+    private SignInButton mySignInButton;
+    private int RC_SIGN_IN;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         myTestAccount = new Account();
+        RC_SIGN_IN = 1;
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        myGoogleSignInClient = GoogleSignIn.getClient(this.getContext(), gso);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this.getContext());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.login_fragment, container, false);
 
-        myEmailLogin = (EditText) v.findViewById(R.id.emailLogin);
-        myEmailLogin.addTextChangedListener(new TextWatcher() {
+        mySignInButton = (SignInButton) v.findViewById(R.id.sign_in_button);
+        mySignInButton.setEnabled(true);
+        mySignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
-                myTestAccount.setEmailAddress(s.toString());
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
+            public void onClick(View v) {
+                signIn();
             }
         });
-
-        myPasswordLogin = (EditText) v.findViewById(R.id.passwordLogin);
-        myPasswordLogin.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
-                myTestAccount.setPassword(s.toString());
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
-
-        myLoginButton = (Button) v.findViewById(R.id.loginButton);
-        myLoginButton.setEnabled(false);
-
-        myCreateAccountButton = (Button) v.findViewById(R.id.createAccountButton);
-
-        myForgotPasswordButton = (Button) v.findViewById(R.id.forgotPasswordButton);
 
         return v;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = myGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
 }
